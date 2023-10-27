dd5c62e22919bbea735d66abc98541df097adb0e
sneha <yysnehalatha@gmail.com>
2023-10-27 05:49:38 +0000

    solution

---
+++ src/main/java/com/example/player/PlayerController.java
@@ -0,0 +1,48 @@
+/*
+ * You can use the following import statements
+ * 
+ * import org.springframework.web.bind.annotation.*;
+ * import java.util.ArrayList;
+ * 
+ */
+
+// Write your code here.
+package com.example.player;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+
+import com.example.player.PlayerService;
+
+
+@RestController
+public class PlayerController {
+
+    PlayerService playerService = new PlayerService();
+
+    @GetMapping("/players")
+    public ArrayList<Player> getPlayersList() {
+        return playerService.getPlayers();
+    }
+
+    @GetMapping("/players/{playerId}")
+    public Player getPlayer(@PathVariable("playerId") int playerId) {
+        return playerService.getPlayer(playerId);
+    }
+
+    @PostMapping("/players")
+    public Player addPlayer(@RequestBody Player player) {
+        return playerService.addPlayer(player);
+    }
+
+    @PutMapping("/players/{playerId}")
+    public Player updatePlayer(@PathVariable("playerId") int playerId, @RequestBody Player player) {
+        return playerService.updatePlayer(playerId, player);
+    }
+
+    @DeleteMapping("/players/{playerId}")
+    public void deletePlayer(@PathVariable("playerId") int playerId) {
+        playerService.deletePlayer(playerId);
+    }
+
+}
